2f220cbc746999b0682e02d7ce497f49c9614cb3
Mac User <macuser@MacBook-Pro-de-Mac.local>
2016-11-14 23:17:21 -0500

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161114234010) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "petitions", force: :cascade do |t|
     t.string   "name"
     t.integer  "identity"
